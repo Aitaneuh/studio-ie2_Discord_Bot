dc2de1aa2d6a8523518148de9c44925bc616fc2a
Aitaneuh <130589742+Aitaneuh@users.noreply.github.com>
2026-01-22 14:02:19 +0100

    Add files via upload

---
+++ utils/api_query.py
@@ -0,0 +1,20 @@
+import httpx
+import asyncio
+from config import API_TOKEN
+
+async def get_player_data(tag):
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    # encoding # to %23
+    clean_tag = tag.replace("#", "%23")
+    url = f"https://api.clashroyale.com/v1/players/{clean_tag}"
+    
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, headers=headers)
+        if resp.status_code == 200:
+            data = resp.json()
+            return {
+                "name": data['name'],
+                "trophies": data['trophies'],
+                "best": data['bestTrophies']
+            }
+        return None
