6e624f2e7d990f38bd025cc372dc5cb04ac547ee
Aitaneuh <130589742+Aitaneuh@users.noreply.github.com>
2026-01-22 14:02:50 +0100

    Enhance registration modal with player data integration
    Refactor registration modal to include player data retrieval and update logic.

--- modals/register_modal.py
+++ modals/register_modal.py
@@ -5,6 +5,7 @@ import config
 from utils.simple_embed import get_simple_embed
 from database.connection import SessionLocal
 from database.repositories.player_repo import PlayerRepository
+from utils.api_query import get_player_data
 
 db = SessionLocal()
 player_repo = PlayerRepository(db)
@@ -23,66 +24,66 @@ class RegisterModal(discord.ui.Modal, title="Enregistrement Clash Royale"):
             max_length=30
         )
 
-        self.cr_pseudo = discord.ui.TextInput(
-            label="Pseudo Clash Royale",
-            placeholder="Ex: Ethan",
-            max_length=30
-        )
-
         self.cr_tag = discord.ui.TextInput(
             label="Tag",
             placeholder="#PQJRVVR98",
             max_length=20
         )
 
-        self.cr_trophies = discord.ui.TextInput(
-            label="Nombre de trophées",
-            placeholder="8000",
-            max_length=10
-        )
 
         self.add_item(self.first_name)
         self.add_item(self.last_name)
-        self.add_item(self.cr_pseudo)
         self.add_item(self.cr_tag)
-        self.add_item(self.cr_trophies)
 
     async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
 
-        # Extract values from the modal
         first_name = self.first_name.value.capitalize()
         last_name = self.last_name.value.capitalize()
-        pseudo = self.cr_pseudo.value
-        tag = self.cr_tag.value.replace("#", "").upper()
-        trophies = self.cr_trophies.value
+        tag = "#" + self.cr_tag.value.replace("#", "").upper()
+        
+        player_data = await get_player_data(tag)
+        if player_data == None:
+            return await interaction.followup.send("Le nombre de trophées doit être un chiffre.", ephemeral=True)
+        
+        pseudo = player_data["name"]
+        trophies = player_data["trophies"]
+        best_trophies = player_data["best"]
 
-        player_role = discord.utils.get(interaction.guild.roles, id=config.ROLES["player"]); # type: ignore
-        player = None
+        player_role = discord.utils.get(interaction.guild.roles, id=config.ROLES["player"])
+        
         try:
-            await interaction.user.add_roles(player_role) # type: ignore
-            await interaction.user.edit(nick=f"{player.first_name} {player.last_name}") # type: ignore
-            player = player_repo.create_player(str(interaction.user.id), first_name, last_name, tag, pseudo, int(trophies))
-        except:
-            pass
-
-        if player:
-            embed = get_simple_embed(title="Enregistrement complété",
-                description=(
-                    f"Merci {player.first_name} {player.last_name}, votre compte Clash Royale a bien été enregistré !\n\n"
-                    f"**Pseudo :** {pseudo}\n"
-                    f"**Tag :** #{tag}\n"
-                    f"**Trophées :** {trophies}"
-                ),
-                color=discord.Color.green())
-        else:
-            embed = get_simple_embed(
-                title="Un bug a eu lieu...",
-                description=(
-                    f"Contactez <@677579030966304769> pour qu'il règle votre problème !"
-                ),
-                color=discord.Color.red()
+            player = player_repo.create_player(
+                str(interaction.user.id), 
+                first_name, 
+                last_name, 
+                tag, 
+                pseudo, 
+                best_trophies
             )
 
-        await interaction.user.send(
-            embed=embed
-        )
+            if player_role:
+                await interaction.user.add_roles(player_role)
+            
+            await interaction.user.edit(nick=f"{pseudo}")
+
+            embed = get_simple_embed(
+                title="Enregistrement complété",
+                description=(
+                    f"Merci {first_name} {last_name}, votre compte Clash Royale a bien été enregistré !\n\n"
+                    f"**Pseudo :** {pseudo}\n"
+                    f"**Tag :** {tag}\n"
+                    f"**Trophées :** {trophies}"
+                ),
+                color=discord.Color.green()
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
+
+        except Exception as e:
+            print(f"Erreur lors de l'enregistrement: {e}")
+            embed = get_simple_embed(
+                title="Un bug a eu lieu...",
+                description="Contactez un'administrateur pour régler votre problème.",
+                color=discord.Color.red()
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
