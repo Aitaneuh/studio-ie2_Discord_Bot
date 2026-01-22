3fbcb516c51260d9d906d384b559fa0faa19036d
Aitaneuh <130589742+Aitaneuh@users.noreply.github.com>
2026-01-22 15:25:16 +0100

    Update player_repo.py

--- database/repositories/player_repo.py
+++ database/repositories/player_repo.py
@@ -5,8 +5,8 @@ class PlayerRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_player(self, discord_id: str, first_name: str, last_name: str, cr_tag: str, cr_username: str, cr_trophy_count: int) -> Player:
-        player = Player(discord_id=discord_id, first_name=first_name, last_name=last_name, cr_tag=cr_tag, cr_username=cr_username, cr_trophy_count=cr_trophy_count, wins=0, losses=0)
+    def create_player(self, discord_id: str, first_name: str, last_name: str, cr_tag: str, cr_username: str, cr_trophy_count: int,acc_wins: int) -> Player:
+        player = Player(discord_id=discord_id, first_name=first_name, last_name=last_name, cr_tag=cr_tag, cr_username=cr_username, cr_trophy_count=cr_trophy_count,acc_wins=acc_wins, wins=0, losses=0)
         self.db.add(player)
         self.db.commit()
         self.db.refresh(player)
